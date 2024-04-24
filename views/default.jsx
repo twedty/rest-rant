ea6bc02f2fadd73a26b252283f6f7adefd3aed5d
twedty <jamestwedt@gmail.com>
2024-04-24 18:19:30 -0400

    found some other issues that were colliding with one another like 2 separate default.jsx files

--- views/default.jsx
+++ views/default.jsx
@@ -1,23 +1,16 @@
 const React = require('react')
-const Def = require('../default')
 
-function index (data) {
-    let placesFormatted = data.places.map((place) => {
-        return (
-            <div key={place.name}>
-            <h2>{place.name}</h2>
-            <img src={place.pic} alt={place.name} />
-            </div>
-        )
-        })
-        return (
-        <Def>
-            <main>
-                <h1>PLACES INDEX PAGE</h1>
-                {placesFormatted}
-            </main>
-        </Def>
+function Def(html) {
+    return (
+        <html>
+            <head>
+                <title>Title</title>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
     )
 }
 
-module.exports = index
+module.exports = Def
