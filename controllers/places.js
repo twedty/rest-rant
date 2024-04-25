bc435b4922570508f89e756ce5690f1082bf0c00
twedty <jamestwedt@gmail.com>
2024-04-24 20:04:26 -0400

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -1,8 +1,23 @@
 const router = require('express').Router()
 const places = require("../models/places")
 
-router.get('/', (req, res) => {
-    res.render('places/index', {places})
+let placesFormatted = data.places.map((place, index) => {
+    return (
+        <div className="col-sm-6">
+            <h2>
+                <a href={`/places/${index}`} >
+                    {place.name}
+                </a>
+            </h2>
+                <p className="text-center">
+                {place.cuisines}
+                </p>
+                <img src={place.pic} alt={place.name} />
+                <p className="text-center">
+                Located in {place.city}, {place.state}
+                </p>
+        </div>
+    )
 })
 
 module.exports = router
