2c05b2615503ee710f839d340e6f2b8217a49613
twedty <jamestwedt@gmail.com>
2024-04-24 20:15:58 -0400

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -20,4 +20,31 @@ let placesFormatted = data.places.map((place, index) => {
     )
 })
 
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+        if (isNaN(id)) {
+        res.render('error404')
+        }
+        else if (!places[id]) {
+        res.render('error404')
+        }
+        else {
+        res.render('places/show', { place: places[id], id })
+        }
+    })
+
+router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+        }
+    else if (!places[id]) {
+        res.render('error404')
+        }
+    else {
+        places.splice(id, 1)
+        res.redirect('/places')
+    }
+})
+
 module.exports = router
