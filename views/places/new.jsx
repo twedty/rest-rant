3df2a98f0f68280aec1d4109ab412f66af4f0b85
twedty <jamestwedt@gmail.com>
2024-05-07 22:48:22 -0400

    solved some crashes and still trying to figure out how to tie in my css better

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -2,42 +2,33 @@ const React = require('react')
 const Def = require('../default')
 
 function new_form() {
-    let message = ''               
-    if (data.message) {
-        message = (
-        <h4 className="alert-danger">
-            {data.message}
-        </h4>
-        )
-    }
     return (
         <Def>
             <main>
                 <h1>Add a New Place</h1>
-                {message}
                 <form method="POST" action="/places">
-                    <div className="form-group">
+                    <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
                         <label htmlFor="name">Place Name</label>
                         <input className="form-control" id="name" name="name" required />
                     </div>
-                    <div className="form-group">
+                    <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
                         <label htmlFor="pic">Place Picture</label>
                         <input className="form-control" id="pic" name="pic" />
                     </div>
-                    <div className="form-group">
+                    <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
                         <label htmlFor="city">City</label>
                         <input className="form-control" id="city" name="city" />
                     </div>
-                    <div className="form-group">
+                    <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
                         <label htmlFor="state">State</label>
                         <input className="form-control" id="state" name="state" />
                     </div>
-                    <div className="form-group">
+                    <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
                         <label htmlFor="cuisines">Cuisines</label>
                         <input className="form-control" id="cuisines" name="cuisines" required />
                     </div>
-                    <div className="form-group">
-                        <label for="founded">Founded Year</label>
+                    <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
+                        <label htmlFor="founded">Founded Year</label>
                         <input className="form-control" id="founded" name="founded" />
                     </div>
                     <input className="btn btn-primary" type="submit" value="Add Place" />
@@ -46,5 +37,6 @@ function new_form() {
         </Def>
     )
 }
+
 
 module.exports = new_form
