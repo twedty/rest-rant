8adba4432f854c91584ba45c10adaefff8bc45b9
twedty <jamestwedt@gmail.com>
2024-05-07 21:24:35 -0400

    changed too much to put into words. posting this to test it out and make sure it works

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
 require('dotenv').config()
 const express = require('express')
 const methodOverride = require('method-override')
-const app = express()
+const mongoose = require('mongoose')
 
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+})
+
+const app = express()
 // Express Settings
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
@@ -25,29 +31,3 @@ app.get('*', (req, res) => {
 
 // Listen for Connections
 app.listen(process.env.PORT)
-
-
-
-// 2. Make a new folder (named for the project)
-
-// 3. Inside the folder create a file called ‘index.js’
-
-// 4. Open my terminal and navigate to the project folder
-
-// 5. Run the command “npm init -y”
-
-// 6. Run the command “npm install express”
-
-// 7. Open up ‘index.js’
-
-// 8. Require express at the top of the file
-
-// 9. Initialize the app variable
-
-// 10. Create the home page route
-//    a. Call app.get()
-//    b. Set ‘/‘ as the path (first arg)
-//    c. Write callback function with req, res
-//    d. Call res.send(‘hello world’) 
-
-// 11. Call app.listen(3000) to keep server open
--- models/places.js
+++ models/places.js
@@ -1,15 +1,12 @@
-places = [{
-	name: 'H-Thai-ML',
-	city: 'Seattle',
-	state: 'WA',
-	cuisines: 'Thai, Pan-Asian',
-	pic: 'http://placekitten.com/g/100/200'
-    }, {
-	name: 'Coding Cat Cafe',
-	city: 'Phoenix',
-	state: 'AZ',
-	cuisines: 'Coffee, Bakery',
-	pic: 'http://placekitten.com/g/100/200'
-}]
+const mongoose = require('mongoose')
 
-module.exports = places;
+const placeSchema = new mongoose.Schema({
+	name: { type: String, required: true },
+	pic: String,
+	cuisines: { type: String, required: true },
+	city: { type: String, default: 'Anytown' },
+	state: { type: String, default: 'USA' },
+	founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
