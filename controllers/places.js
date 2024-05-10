17028d11d8f572e637b63f2c9a1fbab5ee574883
twedty <jamestwedt@gmail.com>
2024-05-09 22:12:00 -0400

    added react, added a way to populate the comments page, added newcomment.jsx

--- controllers/places.js
+++ controllers/places.js
@@ -60,6 +60,35 @@ router.get('/:id', (req, res) => {
     })
 })
 
+router.get('/:id/comment', (req, res) => {
+    console.log(req.body);
+    db.Place.findById(req.params.id)
+    .then(place => {
+        res.render('places/newcomment', { place });
+    })
+})
+
+router.post('/:id/comment', (req, res) => {
+    console.log(req.body)
+    db.Place.findById(req.params.id)
+    .then(place => {
+        db.Comment.create(req.body)
+        .then(comment => {
+            place.comments.push(comment.id)
+            place.save()
+            .then(() => {
+                res.redirect(`/places/${req.params.id}`)
+            })
+        })
+        .catch(err => {
+            res.render('error404')
+        })
+    })
+    .catch(err => {
+        res.render('error404')
+    })
+})
+
 
 router.put('/:id', (req, res) => {
     res.send('PUT /places/:id stub')
