7d478f0f0590e3c0f5def94bc3a126fba3aae544
twedty <jamestwedt@gmail.com>
2024-05-09 21:26:37 -0400

    added the comments in the show page and places.js...code is breaking here

--- controllers/places.js
+++ controllers/places.js
@@ -49,7 +49,9 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', (req, res) => {
     db.Place.findById(req.params.id)
+    .populate('comments')
     .then(place => {
+        console.log(place.comments)
         res.render('places/show', { place })
     })
     .catch(err => {
@@ -57,6 +59,7 @@ router.get('/:id', (req, res) => {
         res.render('error404')
     })
 })
+
 
 router.put('/:id', (req, res) => {
     res.send('PUT /places/:id stub')
--- models/places.js
+++ models/places.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose')
 
 const placeSchema = new mongoose.Schema({
-	name: { type: String, required: true, unique: true },
-	pic: { type: String, default: 'http://placehold.it/500x500.png' },
+	name: { type: String, required: true },
+	pic: { type: String, default: 'http://placekitten.com/350/350'},
 	cuisines: { type: String, required: true },
 	city: { type: String, default: 'Anytown' },
 	state: { type: String, default: 'USA' },
 	founded: {
-    	type: Number,
-    	min: [1673, 'Surely not that old?!'],
-    	max: [new Date().getFullYear(), 'Hey, this year is in the future!']
-	}
+		type: Number,
+		min: [1673, 'Surely not that old?!'],
+		max: [new Date().getFullYear(), 'This is the future!']
+	},
+	comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 })
 
 placeSchema.methods.showEstablished = function() {
